b00ae5d0b2ea884ca4be2bd91bb350bb7fbf0319
unknown <ralphflyasian@gmail.com>
2025-07-21 14:22:09 +0800

    new

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ptx04&j@!_jj^6%0nd^6$h$qo6@qw$v^gplcqzgzcybfd5lt%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
